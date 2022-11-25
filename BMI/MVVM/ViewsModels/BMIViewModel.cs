fcb0c2c11470cd04075aa80b2f8d39f42a1d98ed
ethelpoe <macias.itnl@gmail.com>
2022-11-25 06:06:33 -0600

    Add project files.

---
+++ BMI/MVVM/ViewsModels/BMIViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI.MVVM.ViewsModels
+{
+    internal class BMIViewModel
+    {
+        public Models.BMIModel BMIModel { get; set; }
+        public BMIViewModel() {
+            BMIModel = new Models.BMIModel();
+            BMIModel.Height = 100;
+            BMIModel.Weight = 100;
+        }
+    }
+}
